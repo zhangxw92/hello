81cbd9f56c13a3a2f7cffc35932cdeb62cc1c7ec
zhangxw03 <zhangxw_92@163.com>
2021-01-16 20:38:21 +0800

    gc测试

---
+++ src/main/java/com/athome/gc/Hello.java
@@ -0,0 +1,10 @@
+package com.athome.gc;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        while (true) {
+            byte[] b = new byte[10 * 1024 * 1024];
+        }
+    }
+}
